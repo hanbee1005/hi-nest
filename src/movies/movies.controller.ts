c25a996dc777e17f70e9c5c35e80a6f4219afb28
hanbee1005 <sonhanbi1002@gmail.com>
2021-05-21 10:29:41 +0900

    validationPipe 옵션 추가(whitelist, forbidNonWhitelisted, transform)

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -19,7 +19,7 @@ export class MoviesController {
     }
 
     @Get('/:id')
-    getOne(@Param('id') movieId: string): Movie {
+    getOne(@Param('id') movieId: number): Movie {
         return this.moviesService.getOne(movieId);
     }
 
@@ -29,12 +29,12 @@ export class MoviesController {
     }
 
     @Delete('/:id')
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')
-    patch(@Param('id') movieId: string, @Body() updateData) {
+    patch(@Param('id') movieId: number, @Body() updateData) {
         return this.moviesService.update(movieId, updateData);
     }
 }
